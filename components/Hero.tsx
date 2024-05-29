0601717de1950566d18dbd42c994f55242c23544
Arash <arash8877@gmail.com>
2024-05-29 13:28:10 +0200

    add custom Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  return <div className="hero">
-    <div>
-      <h1></h1>
+  const handleClick = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find, book, rent a car—quick and super easy!</h1>
+        <p className="hero__subtitle">
+          {" "}
+          Streamline your car rental experience with our effortless booking process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleClick}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src=/>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Hero;
