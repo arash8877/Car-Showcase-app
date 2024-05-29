a64719dcdab7c00d404d6f8dfb1bcccf0d7036fd
Arash <arash8877@gmail.com>
2024-05-29 10:49:59 +0200

    modify tailwind.config.ts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return <div className="hero">
+    <div>
+      <h1></h1>
+    </div>
+  </div>;
 };
 
 export default Hero;
