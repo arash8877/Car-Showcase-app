b8f3f0b67ba60c98329fb5c82e9bbaa37f32fd62
Arash <arash8877@gmail.com>
2024-06-03 12:34:06 +0200

    fix some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -25,8 +25,8 @@ export default async function Home({ searchParams }: HomeProps) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction} />
+            {/* <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} /> */}
           </div>
         </div>
 
